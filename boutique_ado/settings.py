347515774b41692e2230b00200eabb0fc51effa7
Conor <conor.ganly@yahoo.com>
2022-12-06 16:11:23 +0000

    Add confirmation email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+
     # Other
     'crispy_forms',
 ]
@@ -174,3 +175,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
